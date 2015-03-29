3b3d8d1c46ab866c5e2d71e151511a4e89da6493
karthik2k2 <karthik2k2@outlook.com>
2015-03-29 13:01:39 +0530

    Three ways to do summary stats

--- week1.R
+++ week1.R
@@ -18,3 +18,13 @@ hist(usda$VitaminC,xlab="Vitamin C(mg)",main="Vitamin C content of food",
 
 #boxplot of sugar
 boxplot(usda$Sugar,main="Boxplot of sugar levels",ylab="Sugar(g)")
+
+#adding variable
+usda$HighCarbs <- as.numeric(usda$Carbohydrate>mean(usda$Carbohydrate,na.rm=TRUE))
+usda$HighSodium <- HighSodium
+str(usda)
+
+#Three ways of doing the same thing!(using base R)
+tapply(usda$VitaminC,usda$HighCarbs,summary,na.rm=TRUE)
+by(usda$VitaminC,usda$HighCarbs,summary,na.rm=TRUE)
+aggregate(usda$VitaminC,by=list(HighCarb=usda$HighCarbs),FUN=summary,na.rm=TRUE)
